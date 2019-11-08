ed6d9c020fdbed0b4baa0ea787abc216cc28e6c7
Victor Ng <victor@crankycoder.com>
2019-11-08 11:51:15 -0500

    added test stubs for modifying the remote settings server

---
+++ tests/test_remote_settings.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+These tests exercise the connector to Remote Settings
+"""
+import pytest
+
+
+@pytest.mark.skip
+def test_create_collection():
+    pass
+
+
+@pytest.mark.skip
+def test_clone_collection():
+    pass
+
+
+@pytest.mark.skip
+def test_update_collection():
+    pass
+
+
+@pytest.mark.skip
+def test_invalid_cfr_fails():
+    pass
