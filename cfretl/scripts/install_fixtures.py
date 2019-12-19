85a3034f0d513dd85f1a8bef5be194912175a80b
Victor Ng <victor@crankycoder.com>
2019-12-18 23:51:55 -0500

    reworked fixture installer for CFR models

--- cfretl/scripts/install_fixtures.py
+++ cfretl/scripts/install_fixtures.py
@@ -7,6 +7,9 @@ from cfretl.remote_settings import FEATURES_LIST
 from cfretl.models import CFRModel
 import random
 from datetime import datetime
+import time
+
+random.seed(time.time())
 
 remote_settings = CFRRemoteSettings()
 
@@ -16,31 +19,32 @@ CFRS = remote_settings.debug_load_cfr()
 CFR_ID_LIST = [r["id"] for r in CFRS]
 
 
-def generate_cfr_cfgdata():
+def generate_cfr_cfgdata(version_code):
     """
     This function will need to be rewritten to parse the
     BQ output table and coerce it into values for RemoteSettings JSON
     blob
     """
-    cfg_data = {}
-    for cfr_id in CFR_ID_LIST:
+    model = CFRModel()
 
-        # TODO: replace this with prior 0
-        p0 = random.random()
+    data = []
+    for idx, cfr_id in enumerate(CFR_ID_LIST):
+        prior_0 = random.random()
+        snip = model.one_cfr(
+            cfr_id,
+            [prior_0, 1 - prior_0],
+            [
+                [random.randint(1, 10000) for i in FEATURES_LIST],
+                [random.randint(1, 10000) for i in FEATURES_LIST],
+            ],
+        )
+        data.append(snip)
 
-        cfg_data[cfr_id] = {"p0": p0, "p1": 1 - p0, "features": []}
-        for f_id in FEATURES_LIST:
-            cfg_data[cfr_id]["features"].append(
-                {"feature_id": f_id, "p0": random.random(), "p1": random.random()}
-            )
-    return cfg_data
+    return model.generate_cfr_model(data, version_code)
 
 
-model = CFRModel()
-json_model = model.generate_cfr_model(generate_cfr_cfgdata(), version_code)
+remote_settings.create_user()
+json_model = generate_cfr_cfgdata(version_code)
 remote_settings.write_models(json_model)
-
-remote_settings.clone_to_cfr_control(CFRS)
-remote_settings.clone_to_cfr_experiment(CFRS)
 print("Wrote out version : {:d}".format(version_code))
 print("=" * 20, datetime.now(), "=" * 20)
